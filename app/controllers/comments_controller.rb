63755ca16c14b544f1bd8c762f7bc8396bd45368
Zac Smith <zrs3141592@gmail.com>
2016-07-06 17:15:38 -0600

    Added / corrected comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,7 +6,7 @@ class CommentsController < ApplicationController
   # POST /comments.json
   def create
     @link = Link.find(params[:link_id])
-    @comment = @link.comment.new(comment_params)
+    @comment = @link.comments.new(comment_params)
     @comment.user = current_user
 
     respond_to do |format|
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
       put "like",     to: "links#upvote"
       put "dislike",  to: "links#downvote"
     end
-    resources :coments
+    resources :comments
   end
 
   root to: "links#index"
